bc4a8cbda70a27ef7223689801e17ca1230a973a
freshie-Dev <hazardpubg1122@gmail.com>
2023-06-27 23:59:53 +0500

    basic emoji dictionary

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 4,
+        emoji: "💅🏼",
+        name: "Nail Polish: Medium-Light Skin Tone",
+        meaning:
+          "The Nail Polish: Medium-Light Skin Tone emoji is a modifier sequence combining 💅 Nail Polish and 🏼 Medium-Light Skin Tone. These display as a single emoji on supported platforms."
+      },
+      {
+        id: 4,
+        emoji: "💅🏼",
+        name: "Nail Polish: Medium-Light Skin Tone",
+        meaning:
+          "The Nail Polish: Medium-Light Skin Tone emoji is a modifier sequence combining 💅 Nail Polish and 🏼 Medium-Light Skin Tone. These display as a single emoji on supported platforms."
+      },
+      {
+        id: 4,
+        emoji: "💅🏼",
+        name: "Nail Polish: Medium-Light Skin Tone",
+        meaning:
+          "The Nail Polish: Medium-Light Skin Tone emoji is a modifier sequence combining 💅 Nail Polish and 🏼 Medium-Light Skin Tone. These display as a single emoji on supported platforms."
+      }
+  ];
+
+  export default emojipedia;
+  
